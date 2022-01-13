ddd24736a569b75348cb6050cc76c2ca0b61d4f7
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-01-13 18:04:58 +0530

    Recursion

---
+++ Recursion/Pattern4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+void pattern(int n)
+{
+    if(n>=0)
+    {
+        pattern(n-1);
+        cout<<"1/"<<pow(3,n)<<" + ";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n); 
+    return 0;
+}
