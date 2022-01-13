66f513976c802b6bc56eb0c34fe146bb1f376d8d
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-01-13 17:16:53 +0530

    Recursion

---
+++ Recursion/Prime_number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+bool prime(int n)
+{
+    if(n==1 || n==0)
+    return false;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        return false;
+    }
+    return true;
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(prime(i))
+        cout<<i<<" ";
+    }
+}
