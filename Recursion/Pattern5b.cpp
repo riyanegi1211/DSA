30a062ee6ab10e10397f52d5f255d27deade8a4f
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-01-13 18:14:23 +0530

    Recursion

---
+++ Recursion/Pattern5b.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+void pattern(int n)
+{
+    if(n>=1)
+    {
+        pattern(n-1);
+        cout<<pow(n,n)<<" + ";
+    }
+   
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
