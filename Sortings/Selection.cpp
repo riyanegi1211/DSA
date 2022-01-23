73bfca7ffd74c86827b4e381cd6dc9d8aac2a097
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-01-23 16:54:25 +0530

    Sorting

---
+++ Sortings/Selection.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sort(int arr[],int n)
+{
+    int i,j,k;
+    for(int i=0;i<n-1;i++)
+    {
+        for(j=k=i;j<n;j++)
+        {
+            if(arr[j]<arr[k])
+            k=j;
+        }
+        swap(arr[i],arr[k]);
+    }
+    for(int m=0;m<n;m++)
+    {
+        cout<<arr[m]<<" ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,n);
+    return 0;
+}
